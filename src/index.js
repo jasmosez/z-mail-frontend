bab22d092240730f52fea869ed20874dd388638b
jasmosez <jas.schaffer@gmail.com>
2020-01-18 20:17:18 -0500

    through friday

--- src/index.js
+++ src/index.js
@@ -1,11 +1,209 @@
-console.log("hello")
+// additional seed data
+dataObj = [
+  { date: "2019-10-19", emailIn: 79, emailOut: 6, inbox: 0 },
+  { date: "2019-10-20", emailIn: 63, emailOut: 23, inbox: 1 },
+  { date: "2019-10-21", emailIn: 44, emailOut: 0, inbox: 0 },
+  { date: "2019-10-22", emailIn: 61, emailOut: 2, inbox: 0 },
+  { date: "2019-10-23", emailIn: 79, emailOut: 6, inbox: 0 },
+  { date: "2019-10-24", emailIn: 63, emailOut: 23, inbox: 0 },
+  { date: "2019-10-25", emailIn: 44, emailOut: 0, inbox: 0 },
+  { date: "2019-10-26", emailIn: 61, emailOut: 2, inbox: 0 },
+  { date: "2019-10-27", emailIn: 57, emailOut: 7, inbox: 0 },
+  { date: "2019-10-28", emailIn: 44, emailOut: 12, inbox: 0 },
+  { date: "2019-10-29", emailIn: 79, emailOut: 6, inbox: 0 },
+  { date: "2019-10-30", emailIn: 63, emailOut: 23, inbox: 0 },
+  { date: "2019-10-31", emailIn: 44, emailOut: 0, inbox: 0 },
+  { date: "2019-11-01", emailIn: 57, emailOut: 7, inbox: 0 },
+  { date: "2019-11-02", emailIn: 44, emailOut: 12, inbox: 0 },
+  { date: "2019-11-03", emailIn: 79, emailOut: 6, inbox: 0 },
+  { date: "2019-11-04", emailIn: 63, emailOut: 23, inbox: 0 },
+  { date: "2019-11-05", emailIn: 44, emailOut: 0, inbox: 0 },
+  { date: "2019-11-06", emailIn: 61, emailOut: 2, inbox: 0 },
+  { date: "2019-11-07", emailIn: 79, emailOut: 6, inbox: 0 },
+  { date: "2019-11-08", emailIn: 63, emailOut: 23, inbox: 0 },
+  { date: "2019-11-09", emailIn: 44, emailOut: 0, inbox: 0 },
+  { date: "2019-11-10", emailIn: 61, emailOut: 2, inbox: 0 },
+  { date: "2019-11-11", emailIn: 57, emailOut: 7, inbox: 0 },
+  { date: "2019-11-12", emailIn: 44, emailOut: 12, inbox: 0 },
+  { date: "2019-11-13", emailIn: 79, emailOut: 6, inbox: 0 },
+  { date: "2019-11-14", emailIn: 63, emailOut: 23, inbox: 0 },
+  { date: "2019-11-15", emailIn: 44, emailOut: 0, inbox: 0 },
+  { date: "2019-11-16", emailIn: 61, emailOut: 2, inbox: 0 },
+  { date: "2019-11-17", emailIn: 57, emailOut: 7, inbox: 0 },
+  { date: "2019-11-18", emailIn: 44, emailOut: 12, inbox: 1 },
+  { date: "2019-11-19", emailIn: 79, emailOut: 6, inbox: 0 },
+  { date: "2019-11-20", emailIn: 63, emailOut: 23, inbox: 1 },
+  { date: "2019-11-21", emailIn: 44, emailOut: 0, inbox: 0 },
+  { date: "2019-11-22", emailIn: 61, emailOut: 2, inbox: 4 },
+  { date: "2019-11-23", emailIn: 79, emailOut: 6, inbox: 0 },
+  { date: "2019-11-24", emailIn: 63, emailOut: 23, inbox: 1 },
+  { date: "2019-11-25", emailIn: 44, emailOut: 0, inbox: 0 },
+  { date: "2019-11-26", emailIn: 61, emailOut: 2, inbox: 4 },
+  { date: "2019-11-27", emailIn: 57, emailOut: 7, inbox: 2 },
+  { date: "2019-11-28", emailIn: 44, emailOut: 12, inbox: 1 },
+  { date: "2019-11-29", emailIn: 79, emailOut: 6, inbox: 0 },
+  { date: "2019-11-30", emailIn: 63, emailOut: 23, inbox: 1 },
+  { date: "2019-12-01", emailIn: 57, emailOut: 7, inbox: 0 },
+  { date: "2019-12-02", emailIn: 44, emailOut: 12, inbox: 0 },
+  { date: "2019-12-03", emailIn: 79, emailOut: 6, inbox: 0 },
+  { date: "2019-12-04", emailIn: 63, emailOut: 23, inbox: 0 },
+  { date: "2019-12-05", emailIn: 44, emailOut: 0, inbox: 0 },
+  { date: "2019-12-06", emailIn: 61, emailOut: 2, inbox: 0 },
+  { date: "2019-12-07", emailIn: 79, emailOut: 6, inbox: 0 },
+  { date: "2019-12-08", emailIn: 63, emailOut: 23, inbox: 0 },
+  { date: "2019-12-09", emailIn: 44, emailOut: 0, inbox: 0 },
+  { date: "2019-12-10", emailIn: 61, emailOut: 2, inbox: 0 },
+  { date: "2019-12-11", emailIn: 57, emailOut: 7, inbox: 0 },
+  { date: "2019-12-12", emailIn: 44, emailOut: 12, inbox: 0 },
+  { date: "2019-12-13", emailIn: 79, emailOut: 6, inbox: 0 },
+  { date: "2019-12-14", emailIn: 63, emailOut: 23, inbox: 0 },
+  { date: "2019-12-15", emailIn: 44, emailOut: 0, inbox: 0 },
+  { date: "2019-12-16", emailIn: 61, emailOut: 2, inbox: 0 },
+  { date: "2019-12-17", emailIn: 57, emailOut: 7, inbox: 0 },
+  { date: "2019-12-18", emailIn: 44, emailOut: 12, inbox: 0 },
+  { date: "2019-12-19", emailIn: 79, emailOut: 6, inbox: 0 },
+  { date: "2019-12-20", emailIn: 63, emailOut: 23, inbox: 0 },
+  { date: "2019-12-21", emailIn: 44, emailOut: 0, inbox: 0 },
+  { date: "2019-12-22", emailIn: 61, emailOut: 2, inbox: 0 },
+  { date: "2019-12-23", emailIn: 79, emailOut: 6, inbox: 0 },
+  { date: "2019-12-24", emailIn: 63, emailOut: 23, inbox: 1 },
+  { date: "2019-12-25", emailIn: 44, emailOut: 0, inbox: 0 },
+  { date: "2019-12-26", emailIn: 61, emailOut: 2, inbox: 4 },
+  { date: "2019-12-27", emailIn: 57, emailOut: 7, inbox: 2 },
+  { date: "2019-12-28", emailIn: 44, emailOut: 12, inbox: 1 },
+  { date: "2019-12-29", emailIn: 79, emailOut: 6, inbox: 0 },
+  { date: "2019-12-30", emailIn: 63, emailOut: 23, inbox: 1 },
+  { date: "2019-12-31", emailIn: 44, emailOut: 0, inbox: 0 },
+  { date: "2020-01-01", emailIn: 57, emailOut: 7, inbox: 2 },
+  { date: "2020-01-02", emailIn: 44, emailOut: 12, inbox: 1 },
+  { date: "2020-01-03", emailIn: 79, emailOut: 6, inbox: 0 },
+  { date: "2020-01-04", emailIn: 63, emailOut: 23, inbox: 1 },
+  { date: "2020-01-05", emailIn: 44, emailOut: 0, inbox: 0 },
+  { date: "2020-01-06", emailIn: 61, emailOut: 2, inbox: 4 },
+  { date: "2020-01-07", emailIn: 79, emailOut: 6, inbox: 0 },
+  { date: "2020-01-08", emailIn: 63, emailOut: 23, inbox: 1 },
+  { date: "2020-01-09", emailIn: 44, emailOut: 0, inbox: 0 },
+  { date: "2020-01-10", emailIn: 61, emailOut: 2, inbox: 4 },
+  { date: "2020-01-11", emailIn: 57, emailOut: 7, inbox: 2 },
+  { date: "2020-01-12", emailIn: 44, emailOut: 12, inbox: 1 },
+  { date: "2020-01-13", emailIn: 79, emailOut: 6, inbox: 0 },
+  { date: "2020-01-14", emailIn: 63, emailOut: 23, inbox: 1 },
+  { date: "2020-01-15", emailIn: 44, emailOut: 0, inbox: 0 },
+  { date: "2020-01-16", emailIn: 61, emailOut: 2, inbox: 4 }
+]
+dataTime = "2020-01-16"
 
-var chart = c3.generate({
-  bindto: '#chart',
-  data: {
-    columns: [
-      ['data1', 30, 200, 100, 400, 150, 250],
-      ['data2', 50, 20, 10, 40, 15, 25]
-    ]
+document.addEventListener('DOMContentLoaded', function(){
+  console.log("DOM loaded")
+
+
+  loginButton = document.getElementById('login')
+
+  loginButton.addEventListener('click', function() {
+    // fetch("http://aee5a30f.ngrok.io/auth/google_oauth2")
+    fetch("http://localhost:4567", {mode: "no-cors" })
+
+    
+  })
+
+  function generateDisplayArrays() {
+    let date = ['x']
+    let emailIn = ['Email In']
+    let emailOut = ['Email Out']
+    let inbox = ['Inbox']
+
+    dataObj.forEach(obj => {
+      date.push(obj.date)
+      emailIn.push(obj.emailIn)
+      emailOut.push(obj.emailOut)
+      inbox.push(obj.inbox)
+    })
+
+    // return array of four arrays: date, emailIn, emailOut, inbox
+    return [date, emailIn, emailOut, inbox]
   }
-});
+
+  function clickHandler(arguments) {
+    debugger
+    const id = arguments[0].id
+    const itemCount = arguments[0].value
+    const date = arguments[0].x
+    const showPanel = document.getElementById('show')
+    showPanel.innerText = `${id} on ${date}`
+    ul = document.createElement('ul')
+    
+    for (let i = 1; i <= itemCount; i++) {
+      li = document.createElement('li')
+      li.innerText = `Message #${i}`
+      ul.appendChild(li)
+    }
+
+    showPanel.appendChild(ul)
+    
+    // value: 4
+    // id: "Inbox"
+    // index: 34
+    // name: "Inbox"
+
+    
+    // for as many messages as we would expect, generate a little table of results
+
+    // mock up gmails for 1 or 2 days
+    // date, label, message
+  }
+
+  const chart = c3.generate({
+    data: {
+      x: 'x',
+      //        xFormat: '%Y%m%d', // 'xFormat' can be used as custom format of 'x'
+      columns: generateDisplayArrays(),
+      onclick: function () {
+        clickHandler(arguments)
+      },
+      axes: {
+        "Inbox": 'y2'
+      },
+      types: {
+        "Inbox": 'bar'
+      }
+    },
+    axis: {
+      x: {
+        type: 'timeseries',
+        tick: {
+          format: '%Y-%m-%d'
+        }
+      },
+      y: {
+        label: {
+          text: 'Daily Throughput',
+          position: 'outer-middle'
+        }
+      },
+      y2: {
+        show: true,
+        label: {
+          text: `Inbox as of ${dataTime}`,
+          position: 'outer-middle'
+        }
+      }
+    }
+  });
+
+
+
+  
+
+})
+
+
+// function onSignIn(googleUser) {
+//   var profile = googleUser.getBasicProfile();
+//   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+//   console.log('Name: ' + profile.getName());
+//   console.log('Image URL: ' + profile.getImageUrl());
+//   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+// }
+
+
+// })
